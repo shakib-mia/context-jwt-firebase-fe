a4807bc1baa4c8f0eaf54ddf13d87e67bae33aa2
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-05-03 12:16:05 +0600

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { app } from './firebase.config';
+import { getAuth } from 'firebase/auth'
 
 function App() {
+  const auth = getAuth(app);
+
   return (
     <>
       <h1 className='text-3xl'>hello</h1>
---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDY_qkBmThmD3v-gYrivSRQamp4u_m956M",
+    authDomain: "understanding-jwt.firebaseapp.com",
+    projectId: "understanding-jwt",
+    storageBucket: "understanding-jwt.appspot.com",
+    messagingSenderId: "543615401535",
+    appId: "1:543615401535:web:082abd817667577576828a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
